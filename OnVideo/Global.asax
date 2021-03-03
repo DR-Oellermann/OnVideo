2de0b3c56510cf87136c867c4ba899a1d09ecce8
DrOellermann <u19057416@tuks.co.za>
2021-03-03 09:06:34 +0200

    Add project files.

---
+++ OnVideo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnVideo.MvcApplication" Language="C#" %>
---
+++ OnVideo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnVideo.Startup))]
+namespace OnVideo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
