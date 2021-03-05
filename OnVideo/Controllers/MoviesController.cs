54068d51b0381f87b490bd8e9bab91b0462769ae
DrOellermann <u19057416@tuks.co.za>
2021-03-05 11:29:26 +0200

    Added Details page for movies and controller actions (Excercise)

--- OnVideo/Controllers/MoviesController.cs
+++ OnVideo/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +10,18 @@ namespace OnVideo.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -21,15 +34,24 @@ namespace OnVideo.Controllers
 
         public ActionResult Index()
         {
-            var movie = new List<Movie>
-            {
-                new Movie{ Name = "Movie 1"},
-                new Movie{ Name = "Movie 2"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).ToList();
 
             //var movie = new Movie() { Name = "Shrek" };
 
             return View(movie);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            var details = _context.Movies.Include(g => g.Genre).SingleOrDefault(x => x.Id == id);
+
+            if (details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(details);
+
         }
 
         //public ActionResult ByReleaseDate(int year, int month)
