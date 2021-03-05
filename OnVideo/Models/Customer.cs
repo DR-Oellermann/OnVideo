76d00133f70bce80931ae04e8ffa923ab2ec3549
DrOellermann <u19057416@tuks.co.za>
2021-03-05 10:53:06 +0200

    Added Birthdate and membership name to details view (Excercise)

--- OnVideo/Models/Customer.cs
+++ OnVideo/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,8 @@ namespace OnVideo.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
 
     }
 }
