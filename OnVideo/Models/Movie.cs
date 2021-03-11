9464e1f5c05d4e47177d6586af30fee3cfbd13d5
DrOellermann <u19057416@tuks.co.za>
2021-03-11 12:49:47 +0200

    added movie validation

--- OnVideo/Models/Movie.cs
+++ OnVideo/Models/Movie.cs
@@ -9,16 +9,22 @@ namespace OnVideo.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's Name.")]
+        [StringLength(255)]
         public string  Name { get; set; }
 
         public Genre Genre { get; set; }
+
         [Display(Name = "Genre")]
-        [Required]
+        [Required(ErrorMessage = "Genre is required!")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
         [Display(Name = "Released Date")]
         public DateTime DateReleased { get; set; }
+
+        [Required(ErrorMessage = "Number in stock required!")]
         [Display(Name = "Stock Level")]
         [Range(1,20)]
         public byte NumberInStock { get; set; }
