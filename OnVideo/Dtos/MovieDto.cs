e1a2467a8001be0ae98f179f85de804991821a77
DrOellermann <u19057416@tuks.co.za>
2021-03-12 14:10:13 +0200

    Created movie dto

---
+++ OnVideo/Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using OnVideo.Models;
+
+namespace OnVideo.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's Name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required(ErrorMessage = "Genre is required!")]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        [Display(Name = "Released Date")]
+        public DateTime DateReleased { get; set; }
+
+        [Required(ErrorMessage = "Number in stock required!")]
+        [Display(Name = "Stock Level")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
