3586ea038f905cdf8ccebfadfd81074fccb45384
DrOellermann <u19057416@tuks.co.za>
2021-03-18 09:22:12 +0200

    Added driving license Nr. field to register and model

--- OnVideo/Models/AccountViewModels.cs
+++ OnVideo/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace OnVideo.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
