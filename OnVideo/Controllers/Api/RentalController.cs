eb6ea4e2cace2e15c47fbef8345778109c6414f9
DrOellermann <u19057416@tuks.co.za>
2021-03-23 11:14:44 +0200

    Updated movie model

--- OnVideo/Controllers/Api/RentalController.cs
+++ OnVideo/Controllers/Api/RentalController.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Web.Http;
 using OnVideo.Dtos;
+using OnVideo.Models;
 
 namespace OnVideo.Controllers.Api
 {
     public class RentalController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers
+                .Single(x => x.Id == newRental.CustomerId);
+
+            var movies = _context.Movies
+                .Where(x => newRental.MovieIds.Contains(x.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            return Ok();
         }
     }
 }
